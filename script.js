d37c0073c49fd0b4bad6d0424b0cccff7d66bc9b
jmelara02 <jesusmelara77@gmail.com>
2023-11-02 16:33:33 +0100

    Plataform
    I have added the platform with movement from left to right.

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const myCanvas = document.getElementById("barra");
+myCanvas.width = WIDTH;
+myCanvas.height = HEIGHT;
+
+const WIDTH = 516;
+const HEIGHT = 550;
+const FACTOR = 0.25;
+
+let context = myCanvas.getContext("2d");
+
+
+const player = {
+    url: "./imagnes/Plataforma.png",
+    x: (WIDTH / 2) - 50,
+    y: HEIGHT - 150,
+    speed: 15
+  };
+  
+  
+  function update() {
+    console.log("update");
+    requestAnimationFrame(render);
+    requestAnimationFrame(update);
+  }
+  
+  function render() {
+    clearRect(context);
+    drawImage(context, player);
+  }
+  
+  addEventListener("keydown", (event) => {
+    let isLeft = event.key === "ArrowLeft";
+    let isRight = event.key === "ArrowRight";
+  
+    if (isLeft) {
+      player.x -= player.speed * FACTOR;
+    }
+    if (isRight) {
+      player.x += player.speed * FACTOR;
+    }
+  });
+
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("myCanvas");
     const backgroundMusic = document.getElementById("backgroundMusic");
