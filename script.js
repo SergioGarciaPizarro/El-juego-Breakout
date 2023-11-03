83cb4b8d782046e8a5ac8b7f75058066733cd2c3
AlexisCortesQuiros <alexiscq2503@gmail.com>
2023-11-03 16:32:01 +0100

    Bloques y paddle
    Creación de primer nivel que genera bloques y una barra

--- script.js
+++ script.js
@@ -1,44 +1,119 @@
-const myCanvas = document.getElementById("barra");
-myCanvas.width = WIDTH;
-myCanvas.height = HEIGHT;
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
 
-const WIDTH = 516;
-const HEIGHT = 550;
-const FACTOR = 0.25;
+const level1 = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  ['R','R','R','R','R','R','R','R','R','R','R','R','R','R'],
+  ['R','R','R','R','R','R','R','R','R','R','R','R','R','R'],
+  ['O','O','O','O','O','O','O','O','O','O','O','O','O','O'],
+  ['O','O','O','O','O','O','O','O','O','O','O','O','O','O'],
+  ['G','G','G','G','G','G','G','G','G','G','G','G','G','G'],
+  ['G','G','G','G','G','G','G','G','G','G','G','G','G','G'],
+  ['Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y'],
+  ['Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y']
+];
 
-let context = myCanvas.getContext("2d");
+const colorMap = {
+  'R': 'red',
+  'O': 'orange',
+  'G': 'green',
+  'Y': 'yellow'
+};
+
+const brickGap = 2;
+const brickWidth = 12;
+const brickHeight = 8;
 
 
-const player = {
-    url: "./imagnes/Plataforma.png",
-    x: (WIDTH / 2) - 50,
-    y: HEIGHT - 150,
-    speed: 15
-  };
-  
-  
-  function update() {
-    console.log("update");
-    requestAnimationFrame(render);
-    requestAnimationFrame(update);
+const wallSize = 12;
+const bricks = [];
+
+
+for (let row = 0; row < level1.length; row++) {
+  for (let col = 0; col < level1[row].length; col++) {
+    const colorCode = level1[row][col];
+
+    bricks.push({
+      x: wallSize + (brickWidth + brickGap) * col,
+      y: wallSize + (brickHeight + brickGap) * row,
+      color: colorMap[colorCode],
+      width: brickWidth,
+      height: brickHeight
+    });
   }
+}
+
+const paddle = {
+ 
+  x: canvas.width / 2 - brickWidth / 2,
+  y: 440,
+  width: brickWidth,
+  height: brickHeight,
+
+  dx: 0
+};
+
+
+function collides(obj1, obj2) {
+  return obj1.x < obj2.x + obj2.width &&
+         obj1.x + obj1.width > obj2.x &&
+         obj1.y < obj2.y + obj2.height &&
+         obj1.y + obj1.height > obj2.y;
+}
+
+
+function loop() {
+  requestAnimationFrame(loop);
+  context.clearRect(0,0,canvas.width,canvas.height);
+
+  paddle.x += paddle.dx;
+
+
   
-  function render() {
-    clearRect(context);
-    drawImage(context, player);
-  }
+  context.fillStyle = 'lightgrey';
+  context.fillRect(0, 0, canvas.width, wallSize);
+  context.fillRect(0, 0, wallSize, canvas.height);
+  context.fillRect(canvas.width - wallSize, 0, wallSize, canvas.height);
+
   
-  addEventListener("keydown", (event) => {
-    let isLeft = event.key === "ArrowLeft";
-    let isRight = event.key === "ArrowRight";
-  
-    if (isLeft) {
-      player.x -= player.speed * FACTOR;
-    }
-    if (isRight) {
-      player.x += player.speed * FACTOR;
-    }
+  bricks.forEach(function(brick) {
+    context.fillStyle = brick.color;
+    context.fillRect(brick.x, brick.y, brick.width, brick.height);
   });
+
+  
+  context.fillStyle = 'cyan';
+  context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+
+document.addEventListener('keydown', function(e) {
+
+  if (e.which === 37) {
+    paddle.dx = -3;
+  }
+ 
+  else if (e.which === 39) {
+    paddle.dx = 3;
+  }
+
+  
+});
+
+
+document.addEventListener('keyup', function(e) {
+  if (e.which === 37 || e.which === 39) {
+    paddle.dx = 0;
+  }
+});
+
+
+requestAnimationFrame(loop);
 
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.getElementById("myCanvas");
